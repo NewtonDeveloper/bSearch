db7ed7e844816a07dbee50c2269217a21cb2095b
NewtonDeveloper <andrzej.revq@gmail.com>
2017-04-10 17:31:30 +0200

    initialize project

---
+++ bsearch.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+typedef struct {
+    double x;
+    double y;
+    double z;
+} vector;
+
+typedef struct {
+    vector vec;
+    double length;
+} ext_vector;
+
+int main() {
+
+}
+
+int compare(const void *first, const void *second) {
+
+}
+
+void calc_length(ext_vector *vec1) {
+    vec1-> = sqrt(pow(vec1->vec.x, 2) + pow(vec
+    ->vec.y, 2) + pow(vec1->vec.z, 2));
+}
